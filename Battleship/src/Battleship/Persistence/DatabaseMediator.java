760838d9ec6d2d5197d8d806ca96a30e31c7e623
Bazzzzz <sebastiaan.koch@hotmail.com>
2016-03-11 14:02:54 +0100

    First Commit
    Some classes, naming of packages.

---
+++ Battleship/src/Battleship/Persistence/DatabaseMediator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Battleship.Persistence;
+
+/**
+ *
+ * @author sebas
+ */
+public class DatabaseMediator {
+    
+}
