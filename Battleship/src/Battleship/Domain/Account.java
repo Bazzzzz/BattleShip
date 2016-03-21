8fe70d64834195fd3310ad2080897a6c78216abd
Bazzzzz <sebastiaan.koch@hotmail.com>
2016-03-21 15:50:41 +0100

    Account class full, Overview start

--- Battleship/src/Battleship/Domain/Account.java
+++ Battleship/src/Battleship/Domain/Account.java
@@ -5,10 +5,71 @@
  */
 package Battleship.Domain;
 
+import java.io.Serializable;
+
 /**
  *
  * @author sebas
  */
-public class Account {
-    
+public class Account implements Serializable {
+
+    private String loginName;
+    private String password;
+
+    /**
+     * Constructs an account.
+     *
+     * @param loginName not null
+     * @param password not null, larger than 4
+     */
+    public Account(String loginName, String password) {
+        if (loginName == null || loginName.equals("")) {
+            throw new IllegalArgumentException("Login name was null.");
+        }
+        if (password == null || password.equals("")) {
+            throw new IllegalArgumentException("Password was null.");
+        }
+        if (password.length() < 4) {
+            throw new IllegalArgumentException("Password has to be larger than 4");
+        }
+        this.loginName = loginName;
+        this.password = password;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Changes the password of the user if the entered original password is
+     * correct.
+     *
+     * @param originalPassword not null
+     * @param newPassword not null, larger than 5
+     * @return True if changed, False if not.
+     */
+    public boolean changePassword(String originalPassword, String newPassword) {
+        if (originalPassword != null && newPassword != null) {
+            if (newPassword.length() < 4) {
+                throw new IllegalArgumentException("Password has to be larger than 4");
+            }
+            if (originalPassword.equals(this.password)) {
+                this.password = newPassword;
+                return true;
+            }
+        } else {
+            throw new IllegalArgumentException("Original password and original password can't be null.");
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Account login name: " + this.loginName;
+    }
 }
--- Battleship/src/Battleship/Domain/Overview.java
+++ Battleship/src/Battleship/Domain/Overview.java
@@ -5,10 +5,35 @@
  */
 package Battleship.Domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author sebas
  */
 public class Overview {
+    private boolean isOpponentsBoard;
+    private List<SpecialPackage> specials;
     
+    public Overview() {
+        this.isOpponentsBoard = false;
+        this.specials = new ArrayList<SpecialPackage>(4);
+    }
+    /**
+     * Checks if a location on the overview is available for action.
+     * @param location not null
+     * @return True if the location is available, False if filled.
+     */
+    public boolean locationAvailable(int[][] location) {
+        return false;
+    }
+    /**
+     * Checks if a location on the overview has a ship.
+     * @param location not null
+     * @return True if the location holds a piece of a ship, False if not.
+     */
+    public boolean locationHasShip(int[][] location) {
+        return false;
+    }
 }
